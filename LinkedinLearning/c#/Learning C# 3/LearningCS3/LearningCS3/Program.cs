160d8f42a17f6cfeccf8f73f966cd8e445fc144f
Mochacycle13 <mmmoore13@gmail.com>
2020-03-01 01:20:02 -0500

    Created practice repository; added file locations for current practice sites. Began LinkedinLearning Learning C# 3. Created a Hello World Function

---
+++ LinkedinLearning/c#/Learning C# 3/LearningCS3/LearningCS3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningCS3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
