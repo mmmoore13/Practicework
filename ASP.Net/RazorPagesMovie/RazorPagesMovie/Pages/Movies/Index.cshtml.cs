94d58c5156890776642fb973f75512a6c5a67036
Mochacycle13 <mmmoore13@gmail.com>
2020-05-18 05:09:27 -0400

    Worked through the bugs, updated the RazorPagesMovie up to the Search Function page. Attempting to add an updated version of System.Web.Mvc.

---
+++ ASP.Net/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using System.Web.Mvc;
+
+namespace RazorPagesMovie.PagesMovies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MvcMovieContext _context;
+
+        public IndexModel(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
